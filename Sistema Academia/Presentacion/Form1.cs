ef8b4de401ebdc3d478f467ec21561764e64eabc
kevinPuscan <kpuscano12@unc.edu.pe>
2016-10-30 21:24:32 -0500

    Agregar app.config y clase conectar

--- Sistema Academia/Presentacion/Form1.cs
+++ Sistema Academia/Presentacion/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Presentacion
 {
@@ -15,6 +16,13 @@ namespace Presentacion
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            SqlConnection conexion = conectar.obtenerConexion();
+            MessageBox.Show("Bienvenido al Sistema de la Academia");
         }
     }
 }
---
+++ Sistema Academia/Presentacion/conectar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentacion.Properties;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Presentacion
+{
+    public class conectar
+    {
+        public static String obtenerCadena() {
+            return Settings.Default.academiaConnectionString;
+        }
+
+        public static SqlConnection obtenerConexion() {
+            SqlConnection con = new SqlConnection(obtenerCadena());
+
+            con.Open();
+            return con;
+        }
+    }
+}
